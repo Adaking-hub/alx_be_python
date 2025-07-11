c685a1cc511959708922fdfc514d85bb1a21cebc
Adaking-hub <obi.chioma478@gmail.com>
2025-07-11 11:38:57 +0100

    Basic python operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addNum = number1 + number2
+subtactNum = number1 - number2
+multiplyNum = number1 * number2
+
+print("Addition of 10 and 5 is: ", addNum)
+print("Subtraction of 10 and 5 is: ", subtactNum)
+print("Multiplication of 10 and 5 is: ", multiplyNum)
