b0c40f153d366bf36f4d8531e8644a543ee3b5fd
Adaking-hub <obi.chioma478@gmail.com>
2025-07-23 13:56:15 +0100

    Python functions and data structures

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+#define a duntion to perform arithmetic operations
+
+def perform_operation(num1, num2, operation):
+    """Perform arithmetic operations based on the given operation type."""
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else:
+        return "Error: Invalid operation. Please choose from add, subtract, multiply, or divide."
+
